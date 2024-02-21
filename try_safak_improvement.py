66c7203c881058422ebd6c3d8dc3a62495ef444f
gemilang <git.gemilang.santiyuda@gmail.com>
2024-02-21 13:17:10 +0800

    add improvement

--- try_safak_improvement.py
+++ try_safak_improvement.py
@@ -20,20 +20,10 @@ if __name__ == "__main__":
     for i in range(len(problem.container_type_list)):
         for j in range(problem.container_type_list[i].num_available):
             solution = add_container(solution, i)
-    cc_idx =  [4,8,9,10]
-    cargo_idx = [i for i in range(len(solution.cargo_dims)) if i not in cc_idx]
-    cargo_idx = list(range(len(solution.cargo_dims)))
-    solution = insert_many_cargo_to_one(solution, np.asanyarray(cc_idx), 0)
     solution = constructive_heuristic(solution)
-    # for i in range(len(solution.container_dims)):
-    #     is_inside_container = solution.cargo_container_maps == i
-    #     visualize_box(solution.container_dims[i], solution.positions[is_inside_container], solution.cargo_dims[is_inside_container], solution.rotation_mats[is_inside_container])
-    
-
-    # improvement
     g = 0.2
     ld = 0.2
-    max_perturb_iter = 100
+    max_perturb_iter = 1000
     max_repair_iter = 10
     best_solution = create_copy(solution)
     solution, best_solution = improvement_heuristic(solution, best_solution, g,ld, max_perturb_iter, max_repair_iter)
